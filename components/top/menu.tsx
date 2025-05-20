6910ec89282f2b890a0e6b2f29dbf998494e62b5
Tickether <tickether@gmail.com>
2025-05-20 18:52:13 +0000

    feat: add privy logout component

--- components/top/menu.tsx
+++ components/top/menu.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { Logout } from "./logout";
 
 
 export function Menu() {
@@ -12,16 +13,19 @@ export function Menu() {
                     <Image src="/icons/512x512.png" alt="logo" width={50} height={50} /> 
                 </div>
 
-                <div className="flex gap-1 items-center">   
-                    <a className="mr-1" href="https://warpcast.com/3wbclub" target="_blank" rel="noopener noreferrer">
-                        <Image src="/icons/farcaster.svg" alt="farcaster" width={25} height={25} />
-                    </a>   
-                    <a href="https://x.com/3wbClub" target="_blank" rel="noopener noreferrer">
-                        <Image src="/icons/x.svg" alt="x" width={30} height={30} />
-                    </a>
-                    <a href="https://t.me/threeWB" target="_blank" rel="noopener noreferrer">
-                        <Image src="/icons/telegram.svg" alt="telegram" width={30} height={30} />
-                    </a>
+                <div className="flex gap-6 items-center">   
+                    <div className="flex gap-1 items-center">
+                        <a className="mr-1" href="https://warpcast.com/3wbclub" target="_blank" rel="noopener noreferrer">
+                            <Image src="/icons/farcaster.svg" alt="farcaster" width={25} height={25} />
+                        </a>   
+                        <a href="https://x.com/3wbClub" target="_blank" rel="noopener noreferrer">
+                            <Image src="/icons/x.svg" alt="x" width={30} height={30} />
+                        </a>
+                        <a href="https://t.me/threeWB" target="_blank" rel="noopener noreferrer">
+                            <Image src="/icons/telegram.svg" alt="telegram" width={30} height={30} />
+                        </a>
+                    </div>
+                    <Logout/>
                 </div>
             </div>
         </div>
