a3f37b3231e4f7b46f771c1b029b7f84c1a3cd16
Tickether <tickether@gmail.com>
2025-06-13 12:11:11 +0000

    feat: add invited + compliant check components

--- components/fleet/wrapper/component.tsx
+++ components/fleet/wrapper/component.tsx
@@ -1,15 +1,55 @@
 "use client"
 
-import { useAccount, useReadContract } from "wagmi"
+import { useAccount, useBlockNumber, useReadContract } from "wagmi"
 import { fleetOrderBook } from "@/utils/constants/addresses"
 import { fleetOrderBookAbi } from "@/utils/abis/fleetOrderBook"
+import { useQueryClient } from "@tanstack/react-query"
+import { useEffect } from "react"
 
 
 
 export function Component() {
+
+    const { address, isConnected } = useAccount()
+
+
+    const invitedQueryClient = useQueryClient() 
+    const compliantQueryClient = useQueryClient()
+    
+    const { data: blockNumber } = useBlockNumber({ watch: true })  
+
+    const { data: whitelisted, isLoading: whitelistedLoading, queryKey: whitelistedQueryKey } = useReadContract({
+        address: fleetOrderBook,
+        abi: fleetOrderBookAbi,
+        functionName: "isWhitelisted",
+        args: [address!],
+    })
+    useEffect(() => { 
+        invitedQueryClient.invalidateQueries({ queryKey: whitelistedQueryKey }) 
+    }, [blockNumber, invitedQueryClient, whitelistedQueryKey]) 
+
+    const { data: compliant, isLoading: compliantLoading, queryKey: compliantQueryKey } = useReadContract({
+        address: fleetOrderBook,
+        abi: fleetOrderBookAbi,
+        functionName: "isCompliant",
+        args: [address!],
+    })
+    useEffect(() => { 
+        compliantQueryClient.invalidateQueries({ queryKey: compliantQueryKey }) 
+    }, [blockNumber, compliantQueryClient, compliantQueryKey]) 
+
+
     return (
         <div>
-            <h1>Component</h1>
+            {
+                whitelistedLoading || compliantLoading 
+                ? <div>Loading...</div> 
+                : (
+                    <>
+                    {}
+                    </>
+                )
+            }
         </div>
     )
 }
