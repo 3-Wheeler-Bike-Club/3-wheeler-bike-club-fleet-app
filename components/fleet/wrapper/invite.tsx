6038d7b1d189c4b683cddf340581675391e9d1f0
Tickether <tickether@gmail.com>
2025-06-14 16:02:24 +0000

    feat: add invite component content

--- components/fleet/wrapper/invite.tsx
+++ components/fleet/wrapper/invite.tsx
@@ -13,6 +13,16 @@ export function Invite() {
                     </AlertDescription>
                 </Alert>
             </div>
+            <div className="flex w-full h-full justify-center">
+                <div className="flex w-full h-full max-w-[66rem] gap-4">
+                    <div className="flex flex-col w-full h-full items-center pt-36 max-md:pt-18 gap-4">
+                        <DoorClosedLocked className="h-40 w-40 max-md:h-30 max-md:w-30 text-yellow-500" />
+                        <p className="text-2xl max-md:text-xl text-center font-bold">Get an Invite.</p>
+                        <p className="text-sm max-md:text-xs text-center text-muted-foreground">Join our <a href="https://t.me/threeWB" target="_blank" rel="noopener noreferrer" className="text-yellow-500 hover:underline">telegram group</a> to get an invite from our community reps.</p>
+                    </div>
+                </div>
+            </div>
+            
         </div>
     )
 }
