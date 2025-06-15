b503824133b4bdf6f8aa02a0723b9146eb13cf32
Tickether <tickether@gmail.com>
2025-06-15 21:04:28 +0000

    feat: add invitation drawer + referral component content

--- components/fleet/wrapper/referrals.tsx
+++ components/fleet/wrapper/referrals.tsx
@@ -1,5 +1,6 @@
 import { Alert, AlertTitle, AlertDescription } from "@/components/ui/alert";
-import { Gift } from "lucide-react";
+import { Gift, UserX } from "lucide-react";
+import { Invitation } from "./invitation";
 
 export function Referrals() {
     return (
@@ -14,6 +15,31 @@ export function Referrals() {
                     </AlertDescription>
                 </Alert>
             </div>
+
+            <div className="flex w-full items-center justify-center">
+                <div className="flex w-full max-w-[66rem] gap-4">
+                    <div className="flex w-full gap-2 justify-between">
+                        <div/>
+                        <div className="flex gap-2">
+                            <Invitation />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="flex w-full items-center justify-center">
+                {true && (
+                    <div className="flex w-full h-full max-w-[66rem] gap-4">
+                        <div className="flex flex-col w-full h-full items-center pt-36 max-md:pt-18 gap-4">
+                            <UserX className="h-40 w-40 max-md:h-30 max-md:w-30 text-yellow-500" />
+                            <p className="text-2xl max-md:text-xl text-center font-bold">No referrals yet.</p>
+                            <p className="text-sm max-md:text-xs text-center text-muted-foreground">Send invites to your friends to start earning rewards.</p>
+                        </div>
+                    </div>
+                    )
+                }
+            </div>
+            
         </div>
     )
 }
