851c7839ef0f5dc01f676f609b768ce599cce3c3
Tickether <tickether@gmail.com>
2025-06-15 16:45:13 +0000

    feat: roundup verify identity drawer component

--- components/fleet/wrapper/verify.tsx
+++ components/fleet/wrapper/verify.tsx
@@ -1,47 +1,20 @@
 "use client"
-import {
-    useState
-  } from "react"
-  import {
-    toast
-  } from "sonner"
-  import {
-    useForm
-  } from "react-hook-form"
-  import {
-    zodResolver
-  } from "@hookform/resolvers/zod"
-  import * as z from "zod"
-  import {
-    cn
-  } from "@/lib/utils"
-  import {
-    Button
-  } from "@/components/ui/button"
-  import {
-    Form,
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-  } from "@/components/ui/form"
-  import {
-    CloudUpload,
-    Paperclip
-  } from "lucide-react"
-  import {
-    FileInput,
-    FileUploader,
-    FileUploaderContent,
-    FileUploaderItem
-  } from "@/components/ui/file-upload"
+
+
 import { Drawer, DrawerContent, DrawerHeader, DrawerTitle, DrawerTrigger, DrawerDescription } from "@/components/ui/drawer"
-  
-  const formSchema = z.object({
-    name_2777197896: z.string()
-  });
+import { z } from "zod"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Button } from "@/components/ui/button"
+import { CloudUpload, Ellipsis, Paperclip } from "lucide-react"
+import { useState } from "react"
+import { toast } from "sonner"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { FileUploader, FileUploaderContent, FileUploaderItem, FileInput } from "@/components/ui/file-upload"
+import { motion } from "framer-motion"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
+
   
 
 
@@ -57,15 +30,20 @@ const FormSchema = z.object({
 
 export function Verify() {
 
-    const [files, setFiles] = useState < File[] | null > (null);
-
-  const dropZoneConfig = {
-    maxFiles: 5,
-    maxSize: 1024 * 1024 * 4,
-    multiple: true,
-  };
+  const [files, setFiles] = useState < File[] | null > (null);
+  const [maxFiles, setMaxFiles] = useState<number | null>(null);
+  console.log(maxFiles);
+  
   const form = useForm < z.infer < typeof FormSchema >> ({
     resolver: zodResolver(FormSchema),
+    defaultValues: {
+      firstname: undefined,
+      lastname: undefined,
+      othername: undefined,
+      email: undefined,
+      id: undefined,
+      files: undefined,
+    },
 
   })
 
@@ -83,77 +61,223 @@ export function Verify() {
     }
   }
 
-    return (
-        <Drawer>
-                <DrawerTrigger asChild>
-                    <Button className="max-w-fit h-12 rounded-2xl">
-                        Complete KYC
-                        {/** <PersonStanding className="text-yellow-600" /> */}
+  return (
+    <Drawer>
+      <DrawerTrigger asChild>
+          <Button className="max-w-fit h-12 rounded-2xl">
+              Complete KYC
+              {/** <PersonStanding className="text-yellow-600" /> */}
+          </Button>
+      </DrawerTrigger>
+      <DrawerContent className="h-full">
+        <div className="mx-auto w-full max-w-sm pb-6">
+          <DrawerHeader>
+              <DrawerTitle>
+                  Verify Your Identity
+              </DrawerTitle>
+              <DrawerDescription className="max-md:text-[0.9rem]">{"Enter Full Name, Scan & Upload your ID."}</DrawerDescription>
+          </DrawerHeader>
+          <div className="flex flex-col p-4">
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+                <FormField
+                  control={form.control}
+                  name="firstname"
+                  render={({ field }) => (
+                      <FormItem>
+                          <div className="flex flex-col gap-1 w-full max-w-sm space-x-2">
+                              <FormLabel className="text-yellow-600">First Name</FormLabel>
+                              <FormControl >
+                                  <Input disabled={ true } className="col-span-3" placeholder={""} {...field} />
+                              </FormControl>
+                          </div>
+                      </FormItem>
+                  )}
+                />
+                <FormField
+                    control={form.control}
+                    name="lastname"
+                    render={({ field }) => (
+                        <FormItem>
+                            <div className="flex flex-col gap-1 w-full max-w-sm space-x-2">
+                                <FormLabel className="text-yellow-600">Last Name</FormLabel>
+                                <FormControl >
+                                    <Input disabled={ true } className="col-span-3" placeholder={""} {...field} />
+                                </FormControl>
+                            </div>
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="othername"
+                    render={({ field }) => (
+                        <FormItem>
+                            <div className="flex flex-col gap-1 w-full max-w-sm space-x-2">
+                                <FormLabel className="text-yellow-600">Other Name</FormLabel>
+                                <FormControl >
+                                    <Input disabled={ true } className="col-span-3" placeholder={""} {...field} />
+                                </FormControl>
+                            </div>
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                        <FormItem>
+                            <div className="flex flex-col gap-1 w-full max-w-sm space-x-2">
+                                <FormLabel className="text-yellow-600">Email</FormLabel>
+                                <FormControl >
+                                    <Input disabled={ true } className="col-span-3" placeholder={""} {...field} />
+                                </FormControl>
+                            </div>
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                  control={form.control}
+                  name="id"
+                  render={({ field }) => (
+                      <FormItem>
+                          <div className="flex flex-col gap-1 w-full max-w-sm space-x-2">
+                              <FormLabel className="text-yellow-600">ID</FormLabel>
+                              {
+                                  !false
+                                  ?(
+                                      <>
+                                          <Select 
+                                            onValueChange={(value) => {
+                                              field.onChange(value);
+                                              // Set maxFiles based on the ID type
+                                              if (value === "passport") {
+                                                setMaxFiles(1); // Only front needed
+                                              } else if (value === "national") {
+                                                setMaxFiles(2); // Front and back needed
+                                              }
+                                            }}
+                                    
+                                            defaultValue={field.value}
+                                          >
+                                              <FormControl>
+                                              <SelectTrigger className="col-span-3">
+                                                  <SelectValue placeholder="Select an ID Type" />
+                                              </SelectTrigger>
+                                              </FormControl>
+                                              <SelectContent className="col-span-3">
+                                                  <SelectItem value="passport">Passport</SelectItem>
+                                                  <SelectItem value="national">National ID</SelectItem>
+                                              </SelectContent>
+                                          </Select>
+                                      </>
+                                  )
+                                  :(
+                                      <>
+                                          <FormControl>
+                                              <Input disabled className="col-span-3" placeholder={""} {...field} />
+                                          </FormControl>
+                                      </>
+                                  )
+                              }
+                          </div>
+                      </FormItem>
+                  )}
+                />
+                {
+                  maxFiles && ( 
+                    <>
+                      <FormField
+                        control={form.control}
+                        name="files"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel className="text-yellow-600">Upload ID <span className="text-xs text-muted-foreground">(must be under 1MB)</span></FormLabel>
+                            <FormControl>
+                              <FileUploader
+                                value={files}
+                                onValueChange={setFiles}
+                                dropzoneOptions={{
+                                  maxFiles: maxFiles,
+                                  maxSize: 1024 * 1024 * 1,
+                                  multiple: true,
+                                  accept: {
+                                    "image/*": [".png", ".jpg", ".jpeg"],
+                                  },
+                                }}
+                                className="relative bg-background rounded-lg p-2"
+                              >
+                                <FileInput
+                                  id="fileInput"
+                                  className="outline-dashed outline-1 outline-slate-500"
+                                >
+                                  <div className="flex items-center justify-center flex-col p-8 w-full ">
+                                    <CloudUpload className='text-gray-500 w-10 h-10' />
+                                    <p className="mb-1 text-sm text-gray-500 dark:text-gray-400">
+                                      <span className="font-semibold">Click to upload </span>
+                                      or drag and drop
+                                    </p>
+                                    <p className="text-xs text-gray-500 dark:text-gray-400">
+                                      PNG, JPG or JPEG
+                                    </p>
+                                  </div>
+                                </FileInput>
+                                <FileUploaderContent>
+                                  {files &&
+                                    files.length > 0 &&
+                                    files.map((file, i) => (
+                                      <FileUploaderItem key={i} index={i}>
+                                        <Paperclip className="h-4 w-4 stroke-current" />
+                                        <span>{file.name}</span>
+                                      </FileUploaderItem>
+                                    ))}
+                                </FileUploaderContent>
+                              </FileUploader>
+                            </FormControl>
+                            <FormDescription className="text-xs text-muted-foreground text-center">{maxFiles === 1 ? "Upload the Front Photo of your Passport" : "Upload the Front and Back of your National ID"}</FormDescription>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                    </> 
+                  )
+                }
+                <div className="flex justify-between">
+                    <Button
+                        className="w-36"
+                        disabled={true}
+                        type="submit"
+                    >
+                        {
+                            false
+                            ? (
+                                <>
+                                    <motion.div
+                                    initial={{ rotate: 0 }} // Initial rotation value (0 degrees)
+                                    animate={{ rotate: 360 }} // Final rotation value (360 degrees)
+                                    transition={{
+                                        duration: 1, // Animation duration in seconds
+                                        repeat: Infinity, // Infinity will make it rotate indefinitely
+                                        ease: "linear", // Animation easing function (linear makes it constant speed)
+                                    }}
+                                >
+                                        <Ellipsis/>
+                                    </motion.div>
+                                </>
+                            )
+                            : (
+                                <>
+                                    Save changes
+                                </>
+                            )
+                        }
                     </Button>
-                </DrawerTrigger>
-                <DrawerContent className="h-full">
-                <div className="mx-auto w-full max-w-sm pb-6">
-                    <DrawerHeader>
-                        <DrawerTitle>
-                            Verify Your Identity
-                        </DrawerTitle>
-                        <DrawerDescription className="max-md:text-[0.9rem]">Enter Full Name, Scan & Upload your ID.</DrawerDescription>
-                    </DrawerHeader>
-                    <div className="flex flex-col p-4">
-                      <Form {...form}>
-                        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 max-w-3xl mx-auto py-10">
-                          
-                              <FormField
-                                control={form.control}
-                                name="files"
-                                render={({ field }) => (
-                                  <FormItem>
-                                    <FormLabel>Select File</FormLabel>
-                                    <FormControl>
-                                      <FileUploader
-                                        value={files}
-                                        onValueChange={setFiles}
-                                        dropzoneOptions={dropZoneConfig}
-                                        className="relative bg-background rounded-lg p-2"
-                                      >
-                                        <FileInput
-                                          id="fileInput"
-                                          className="outline-dashed outline-1 outline-slate-500"
-                                        >
-                                          <div className="flex items-center justify-center flex-col p-8 w-full ">
-                                            <CloudUpload className='text-gray-500 w-10 h-10' />
-                                            <p className="mb-1 text-sm text-gray-500 dark:text-gray-400">
-                                              <span className="font-semibold">Click to upload</span>
-                                              &nbsp; or drag and drop
-                                            </p>
-                                            <p className="text-xs text-gray-500 dark:text-gray-400">
-                                              SVG, PNG, JPG or GIF
-                                            </p>
-                                          </div>
-                                        </FileInput>
-                                        <FileUploaderContent>
-                                          {files &&
-                                            files.length > 0 &&
-                                            files.map((file, i) => (
-                                              <FileUploaderItem key={i} index={i}>
-                                                <Paperclip className="h-4 w-4 stroke-current" />
-                                                <span>{file.name}</span>
-                                              </FileUploaderItem>
-                                            ))}
-                                        </FileUploaderContent>
-                                      </FileUploader>
-                                    </FormControl>
-                                    <FormDescription>Select a file to upload.</FormDescription>
-                                    <FormMessage />
-                                  </FormItem>
-                                )}
-                              />
-                          <Button type="submit">Submit</Button>
-                        </form>
-                      </Form>
-                    </div>              
                 </div>
-                </DrawerContent>
-        </Drawer>
-    );
+              </form>
+            </Form>
+          </div>              
+        </div>
+      </DrawerContent>
+    </Drawer>
+  );
 }
