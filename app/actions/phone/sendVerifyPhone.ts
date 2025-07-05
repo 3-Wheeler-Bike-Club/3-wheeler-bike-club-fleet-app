a832fe6c8c58b2c2024359e0ea36167d65e635e1
Tickether <tickether@gmail.com>
2025-07-05 01:32:02 +0000

    feat: Add phone verification via Twilio
    Introduced phone verification actions using Twilio, including sending and verifying phone codes via WhatsApp. Updated environment types to include Twilio credentials. Also updated email verification variable names for clarity. Added 'twilio' dependency to package.json.

---
+++ app/actions/phone/sendVerifyPhone.ts
@@ -0,0 +1,21 @@
+"use server";
+
+// Download the helper library from https://www.twilio.com/docs/node/install
+import twilio from "twilio";
+
+// Find your Account SID and Auth Token at twilio.com/console
+// and set the environment variables. See http://twil.io/secure
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = twilio(accountSid, authToken);
+
+export async function sendVerifyPhone( phoneNumber: string ) {
+  const verification = await client.verify.v2
+    .services(process.env.TWILIO_SERVICE_SID)
+    .verifications.create({
+      channel: "whatsapp",
+      to: phoneNumber,
+    });
+
+  console.log(verification.status);
+}
---
+++ app/actions/phone/verifyPhoneCode.ts
@@ -0,0 +1,19 @@
+// Download the helper library from https://www.twilio.com/docs/node/install
+import twilio from "twilio";
+
+// Find your Account SID and Auth Token at twilio.com/console
+// and set the environment variables. See http://twil.io/secure
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = twilio(accountSid, authToken);
+
+export async function verifyPhoneCode( code: string, phoneNumber: string ) {
+  const verificationCheck = await client.verify.v2
+    .services(process.env.TWILIO_SERVICE_SID)
+    .verificationChecks.create({
+      code: code,
+      to: phoneNumber,
+    });
+
+  console.log(verificationCheck.status);
+}
